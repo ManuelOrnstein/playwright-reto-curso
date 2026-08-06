4d27984112e58e31f5c4487b3868303114cbb3bc
ManuelOrnstein <gibdon9508@gmail.com>
2026-08-05 22:46:19 -0600

    Reto de dia 1 completado

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,3 +9,13 @@ test('Login to hrm', async({page})=> {
 
     await expect(page.getByRole('link',{name:'Admin'})).toBeVisible()
 })
+
+test('Login to hrm erroneo', async({page})=> {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox',{name:'Username'}).fill('Admin')
+    await page.getByRole('textbox',{name:'Password'}).fill('aqwer')
+    await page.getByRole('button',{name:'Login'}).click() 
+
+    await expect(page.getByRole('alert',{name:''})).toBeVisible()
+})
